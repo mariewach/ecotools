88647532f205a83c7a3c19a506e3133dab33e751
mariewach <marie.wach@gmail.com>
2018-04-25 09:14:00 +0200

    modifications fonction gestion repertoire.R

--- R/gestion_repertoire.R
+++ R/gestion_repertoire.R
@@ -1,11 +1,35 @@
 #' Additions
 #'
 #' Longue description du package
-#' @param rep nom du repetoire ou se trouvent le donnees
-#' @param file nom du fichier avec extension .Rdata
-#' @return importe le fichier rdata
+#' @param name identifiant de la personne, de la forme "prenom.nom"
+#' @param name_project nom du projet
+#' @return les chemins d'accès aux repertoires
 #' @examples
+#' # Créeer le repertoire er sous repertoires pour travailler dans un projets
+#' new_project(name="prenom.nom", name_project = "projetR")
 #'
 #' @export
 
-import_data = function(rep, file){load(paste(rep,file,sep=""))}
+new_project = function(name, name_project){
+
+path = "//filer1.bordeaux.cemagref.fr/Redirect$/"
+
+dir.create(paste(path,name,"/Documents/",name_project,sep=""))
+dir.create(paste(path,name,"/Documents/",name_project,"/input/",sep=""))
+dir.create(paste(path,name,"/Documents/",name_project,"/output/",sep=""))
+dir.create(paste(path,name,"/Documents/",name_project,"/scripts/",sep=""))
+dir.create(paste(path,name,"/Documents/",name_project,"/documents/",sep=""))
+
+main_path = paste(path,name,"/Documents/",name_project,sep="")
+input = paste(path,name,"/Documents/",name_project,"/input/",sep="")
+output = paste(path,name,"/Documents/",name_project,"/output/",sep="")
+scripts = paste(path,name,"/Documents/",name_project,"/scripts/",sep="")
+
+return(main_path, input,output,scripts)
+
+
+}
+
+
+
+
